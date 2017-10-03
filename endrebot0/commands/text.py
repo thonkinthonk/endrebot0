743bb35e32bec92a95065d14241a71347710ce11
Ian W <ian.uu12@gmail.com>
2017-10-02 21:44:20 -0700

    Text manipulation: boxes, arrows, and boxes with x's

--- endrebot0/commands/text.py
+++ endrebot0/commands/text.py
@@ -19,3 +19,34 @@ async def sed_sub(bot, message):
 	if message.content != new_content:
 		await msg.edit(content=new_content)
 	await message.delete()
+
+@command
+def box(ctx, text):
+	text = text.upper().replace(' ', '')
+	top = ' '.join(text)
+	center_spacing = ' ' * (2 * len(text) - 3)
+	sides = (left + center_spacing + right for left, right in zip(text[1:-1], reversed(text[1:-1])))
+	return '\n'.join(('```', top, *sides, ''.join(reversed(top)), '```'))
+
+@command
+def arrow(ctx, text, dir=0):
+	left, up = not (dir % 2), not (dir >> 1)
+	reverse_diag = left ^ up
+	text = text.upper().replace(' ', '')
+	rows = [[' ' for _ in text] for _ in text] # Grid of spaces to fill in
+	rows[0 if up else -1][:] = list(text)
+	for i, (row, letter) in enumerate(zip(rows, reversed(text) if reverse_diag else text)):
+		row[-i-1 if reverse_diag else i] = letter
+		rows[i][0 if left else -1] = letter
+	return '\n'.join(('```', *map(' '.join, rows), '```'))
+
+@command
+def xbox(ctx, text):
+	text = text.upper().replace(' ', '')
+	rows = [[' ' for _ in text + '  '] for _ in text + '  '] # Add two extra rows and columns
+	rows[0][1:-1] = text
+	for i, (row, letter, rev_letter) in enumerate(zip(rows[1:-1], text, reversed(text))):
+		row[0], row[-1], row[i+1] = (letter,) * 3
+		row[-i-2] = rev_letter
+	rows[-1][1:-1] = text
+	return '\n'.join(('```', *map(' '.join, rows), '```'))
