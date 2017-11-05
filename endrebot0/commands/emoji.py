d5d0bcffa9aa177e82f3a45cb2f63e238a295cf8
Ian W <ian.uu12@gmail.com>
2017-11-04 18:13:44 -0700

    Case-sensitive emoji; emoji-copy command

--- endrebot0/commands/emoji.py
+++ endrebot0/commands/emoji.py
@@ -1,4 +1,4 @@
-import re
+import re, requests
 from ..command import *
 
 @on('ready')
@@ -6,9 +6,9 @@ def init(bot):
 	global emoji, emoji_re, emoji_sub
 	emoji = bot.config['emoji']
 	enclosure = emoji.get('_enclosure', ':') # The character that encloses the emoji - intentionally unescaped so config can use regexes
-	emoji_re = re.compile('{0}\s*({1})\s*{0}'.format(enclosure, '|'.join(map(re.escape, emoji))), re.IGNORECASE)
+	emoji_re = re.compile('{0}\s*({1})\s*{0}'.format(enclosure, '|'.join(map(re.escape, emoji))))
 	def emoji_sub(match):
-		return emoji[match.group(1).strip().lower()]
+		return emoji[match.group(1).strip()]
 
 @on('message')
 async def insert_emoji(bot, message):
@@ -16,3 +16,9 @@ async def insert_emoji(bot, message):
 	new = emoji_re.sub(emoji_sub, old)
 	if old != new:
 		await message.edit(content=new)
+
+@command
+async def copy_emoji(ctx, emoji, name=None):
+	with requests.get(emoji.url) as image_data:
+		new_emoji = await ctx.guild.create_custom_emoji(name=name or emoji.name, image=image_data.content)
+	return new_emoji
