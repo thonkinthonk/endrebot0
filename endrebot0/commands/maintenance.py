faf28ec8f75e146c906ee9c592bae50967633255
Ian W <ian.uu12@gmail.com>
2017-11-04 18:14:28 -0700

    Clear listeners between reloads; run on_ready listener on reload

--- endrebot0/commands/maintenance.py
+++ endrebot0/commands/maintenance.py
@@ -7,17 +7,22 @@ async def shutdown(ctx):
 	await ctx.bot.close()
 
 @command
-def reload(ctx, *modules):
+async def reload(ctx, *modules):
 	for mod in modules:
 		if mod == 'config':
 			with open('config.json', encoding='utf-8') as f:
 				ctx.bot.config = json.load(f)
 		else:
-			module = sys.modules['endrebot0.commands.%s' % mod]
-			if hasattr(module, 'listeners'):
-				del module.listeners
-			if hasattr(module, 'commands'):
-				del module.commands
+			module_name = 'endrebot0.commands.{}'.format(mod)
+			module = sys.modules.get(module_name)
+			if module is not None:
+				if hasattr(module, 'listeners'):
+					del module.listeners
+				if hasattr(module, 'commands'):
+					del module.commands
 			importlib.reload(module)
 			ctx.bot.add_module(module)
+			if hasattr(module, 'listeners'):
+				for listener in module.listeners.get('on_ready', ()):
+					await listener(ctx.bot)
 	return 'Reloaded %d module(s)' % len(modules)
